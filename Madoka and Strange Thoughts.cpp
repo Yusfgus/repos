d4799677d094ecba1d54667e2be2fbef3da81fc0
Yousef Mohamed <yousefgus111@gmail.com>
2022-09-03 07:28:41 +0200

    Add c++ file

---
+++ Madoka and Strange Thoughts.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	long long  t, n, ctr;
+	cin >> t;
+	while (t--)
+	{
+		cin >> n;
+		
+		ctr = (n / 3) * 2 + (n / 2) * 2 + n;
+
+		cout << ctr << endl;
+	}
+
+	return 0;
+} 
